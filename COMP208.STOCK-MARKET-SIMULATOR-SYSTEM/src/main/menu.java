ac3f99fa628cf4f72ec53ae5230d99937d6aedac
jinwei.zhang <janviezhang14@gmail.com>
2017-04-03 22:16:06 +0100

    MORE commands

--- COMP208.STOCK-MARKET-SIMULATOR-SYSTEM/src/main/menu.java
+++ COMP208.STOCK-MARKET-SIMULATOR-SYSTEM/src/main/menu.java
@@ -3,6 +3,7 @@ package main;
 public class menu {
  public static void main(String[] args) {
 	System.out.println("this is main menu!");
-	//should be a fully menu
+	//should be a fully menu when ever you make change you should commit the file
+	//at the end of the day you have to push from the local repository to the remote
 }
 }
