7026dc5228645c5cf44c4ee2978a97a105f3aac7
jinwei.zhang <janviezhang14@gmail.com>
2017-04-15 13:38:42 +0100

    all the UI file

---
+++ COMP208.STOCK-MARKET-SIMULATOR-SYSTEM/src/UI/javafxapplication/LoginController.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication;
+
+import com.jfoenix.controls.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+//import com.restfb.DefaultFacebookClient;
+//import com.restfb.FacebookClient;
+//import com.restfb.types.User;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+/**
+ *
+ * @author 进伟
+ */
+public class LoginController extends AnchorPane implements Initializable {//implements Initializable
+
+    @FXML
+    private JFXTextField userName;
+    @FXML
+    private JFXPasswordField password;
+    @FXML
+    private JFXButton login, signUp;
+    @FXML
+    private Label errorMessage;
+    
+    @FXML
+    private AnchorPane loginPane;
+    
+    
+    private Main application;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        
+    }
+    
+    @FXML
+    public void loginButton(ActionEvent event) {
+ 
+        if (userName.getText().equals("zhang") && password.getText().equals("jinwei")) {
+            errorMessage.setText("welcome " + userName.getText());
+            // e -> primaryStage.setScene(scene2);
+
+        } else {
+
+            errorMessage.setText("Username/Password is incorrect");
+        }
+
+
+    }
+
+    @FXML
+    public void signinButtonClick() {
+        System.out.println("user sign in");
+
+    }
+
+    @FXML
+    public void enteredPassword() {
+        // loginButtonClick();
+    }
+
+}
