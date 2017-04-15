d172dc8b35a79b7196313065d6d6a9b48dac66b5
zjpd <18168973150@163.com>
2017-04-15 12:37:17 +0100

    Update of Database

--- COMP208.STOCK-MARKET-SIMULATOR-SYSTEM/src/Database/Database.java
+++ COMP208.STOCK-MARKET-SIMULATOR-SYSTEM/src/Database/Database.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import Server.ErrorExecutor;
+
 public class Database {
 	
 	private static final String url = "jdbc:mysql://127.0.0.1/StockInformation?autoReconnect=true&useSSL=false";
@@ -33,11 +35,15 @@ public class Database {
 	 * @return return the result of the sql instruction
 	 * @throws SQLException if the connection is failed or the creation of the table is failed
 	 */
-	public static synchronized ArrayList<String> createTable(String sql) throws SQLException{
+	public static synchronized ArrayList<String> createTable(String sql){
 		ArrayList<String> sqlreturn = new ArrayList<String>();
-		pst = connect.prepareStatement(sql);
-		pst.execute();
-		result.close();
+		try {
+			pst = connect.prepareStatement(sql);
+			pst.execute();
+			result.close();
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
+		}
 		return sqlreturn;
 	}
 	
@@ -49,7 +55,7 @@ public class Database {
 			connect.close();
 			pst.close();
 		}catch(SQLException e){
-			e.printStackTrace();
+			ErrorExecutor.writeError(e.getMessage());
 		}
 	}
 	
@@ -58,10 +64,14 @@ public class Database {
 	 * @param sql the sql instruction provided by the invoker
 	 * @throws SQLException
 	 */
-	public static synchronized void insert(String sql) throws SQLException {
-		pst = connect.prepareStatement(sql);
-		pst.execute();
-		result.close();
+	public static synchronized void insert(String sql) {
+		try {
+			pst = connect.prepareStatement(sql);
+			pst.execute();
+			result.close();
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
+		}
 	}
 	
 	/**
@@ -70,10 +80,14 @@ public class Database {
 	 * @return true or false
 	 * @throws SQLException
 	 */
-	public static boolean tableExist(String tablename) throws SQLException{
-		result = connect.getMetaData().getTables(null, null, tablename, null);
-		if(result.next()){
-			return true;
+	public static boolean tableExist(String tablename) {
+		try {
+			result = connect.getMetaData().getTables(null, null, tablename, null);
+			if(result.next()){
+				return true;
+			}
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
 		}
 		return false;
 	}
@@ -83,52 +97,76 @@ public class Database {
 	 * @param sql
 	 * @throws SQLException
 	 */
-	public static synchronized void dropTable(String sql) throws SQLException{
-		pst = connect.prepareStatement(sql);
-		pst.execute();
-	}
-	
-	public static synchronized boolean update(String sql) throws SQLException{
-		pst = connect.prepareStatement(sql);
-		int result = pst.executeUpdate(sql);
-		return result == 1 ? true:false;
-	}
-	
-	public static double[] queryStockShare(String sql) throws SQLException {
-		pst = connect.prepareStatement(sql);
-		result = pst.executeQuery(sql);
-		double[] tmp = new double[2];
-		if(result.next()){
-			tmp[0] = Double.valueOf(result.getString(1));
-			tmp[1] = Double.valueOf(result.getString(2));
-		}else{
-			tmp[0]=0;
-			tmp[1]=0;
+	public static synchronized void dropTable(String sql) {
+		try {
+			pst = connect.prepareStatement(sql);
+			pst.execute();
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
 		}
-		return tmp;
 	}
 	
-	public static List<String> queryPersonInfo(String sql) throws SQLException{
-		pst = connect.prepareStatement(sql);
+	public static synchronized boolean update(String sql) {
+		try {
+			pst = connect.prepareStatement(sql);
+			int result = pst.executeUpdate(sql);
+			return result == 1 ? true:false;
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
+		}
+		return false;
+	}
+	
+	public static double[] queryStockShare(String sql) {
+		try {
+			pst = connect.prepareStatement(sql);
+			result = pst.executeQuery(sql);
+			double[] tmp = new double[2];
+			if(result.next()){
+				tmp[0] = Double.valueOf(result.getString(1));
+				tmp[1] = Double.valueOf(result.getString(2));
+			}else{
+				tmp[0]=0;
+				tmp[1]=0;
+			}
+			return tmp;
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
+			return null;
+		}
+		
+	}
+	
+	public static List<String> queryPersonInfo(String sql) {
 		ArrayList<String> sqlreturn = new ArrayList<String>();
-		result = pst.executeQuery(sql);
-		for(int i=1; i<=5; i++){
-			sqlreturn.add(result.getString(i));
+		try {
+			pst = connect.prepareStatement(sql);
+			result = pst.executeQuery(sql);
+			for(int i=1; i<=5; i++){
+				sqlreturn.add(result.getString(i));
+			}
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
 		}
 		return sqlreturn;
 	}
 	
-	public static List<List<String>> queryPersonStock(String sql) throws SQLException{
-		pst = connect.prepareStatement(sql);
+	public static List<List<String>> queryPersonStock(String sql) {
 		List<List<String>> sqlreturn = new ArrayList<List<String>>();
-		result = pst.executeQuery();
-		while(result.next()){
-			List<String> stock = new ArrayList<String>();
-			stock.add(result.getString(1));
-			stock.add(result.getString(2));
-			stock.add(result.getString(3));
-			stock.add(result.getString(4));
-			sqlreturn.add(stock);
+		try {
+			pst = connect.prepareStatement(sql);
+
+			result = pst.executeQuery();
+			while(result.next()){
+				List<String> stock = new ArrayList<String>();
+				stock.add(result.getString(1));
+				stock.add(result.getString(2));
+				stock.add(result.getString(3));
+				stock.add(result.getString(4));
+				sqlreturn.add(stock);
+			}
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
 		}
 		return sqlreturn;
 	}
@@ -138,16 +176,20 @@ public class Database {
 	 * @return
 	 * @throws SQLException
 	 */
-	public static List<String> getStockCode() throws SQLException {
+	public static List<String> getStockCode() {
 		String sql = "select stock_id from Stock";
 		ArrayList<String> sqlreturn = new ArrayList<String>();
-		pst = connect.prepareStatement(sql);
-		result = pst.executeQuery();
-		while(result.next()){
-			String sqlres = result.getString(1);
-			sqlreturn.add(sqlres);
+		try {
+			pst = connect.prepareStatement(sql);
+			result = pst.executeQuery();
+			while(result.next()){
+				String sqlres = result.getString(1);
+				sqlreturn.add(sqlres);
+			}
+			result.close();
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
 		}
-		result.close();
 		return sqlreturn;
 	}
 	
@@ -157,18 +199,22 @@ public class Database {
 	 * @return the list of the stock price which includes "high, low, open and close"
 	 * @throws SQLException
 	 */
-	public static List<Float[]> getStockHisPrice(String stockCode) throws SQLException {
+	public static List<Float[]> getStockHisPrice(String stockCode) {
 		String sql = "select high,low,open,close from " +stockCode;
 		Float[] prices = new Float[4];
 		List<Float[]> returnlist = new ArrayList<Float[]>();
-		pst = connect.prepareStatement(sql);
-		result = pst.executeQuery();
-		while(result.next()){
-			prices[0] = Float.valueOf(result.getString(1)); //1 represent high
-			prices[1] = Float.valueOf(result.getString(2)); //2 represent low
-			prices[2] = Float.valueOf(result.getString(3)); //3 represent open
-			prices[3] = Float.valueOf(result.getString(4)); //4 represent close
-			returnlist.add(prices);
+		try {
+			pst = connect.prepareStatement(sql);
+			result = pst.executeQuery();
+			while(result.next()){
+				prices[0] = Float.valueOf(result.getString(1)); //1 represent high
+				prices[1] = Float.valueOf(result.getString(2)); //2 represent low
+				prices[2] = Float.valueOf(result.getString(3)); //3 represent open
+				prices[3] = Float.valueOf(result.getString(4)); //4 represent close
+				returnlist.add(prices);
+			}
+		} catch (SQLException e) {
+			ErrorExecutor.writeError(e.getMessage());
 		}
 		return returnlist;
 	}
